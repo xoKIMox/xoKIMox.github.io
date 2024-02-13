20515836e1868d9356435b304f1f380c1e3464eb
xoKIMox <Khanthaporm@gmail.com>
2024-02-13 17:20:59 +0700

    รอบ5

---
+++ streamlit/app02.py
@@ -0,0 +1,11 @@
+# streamlit run streamlit/app02.py
+import streamlit as st
+st.sidebar.text('Sidebar')
+st.sidebar.button('go>')
+st.sidebar.button('went>')
+st.sidebar.button('gone>')
+st.title('OOP in ML')
+st.image('https://picsum.photos/500/200')
+st.text_input("What's your name?")
+# git config --global user.name ninasombat
+# git config --global user.email nina.sombat@gmail.com
